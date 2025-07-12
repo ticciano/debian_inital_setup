188d00375c8673e9a30600cbcea0ec471c58b9bf
Ticiano Diniz <ticciano@gmail.com>
2025-07-12 12:02:25 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 sudo apt install -y \
-vim terminator python3 \
-zsh curl git fzf nmap htop \
-jupyter-notebook \
-python3-numpy python3-pandas \
-python3-sklearn \
-copyq flameshot variety
+vim tilix python3 \
+zsh curl git fzf nmap btop \
+copyq flameshot podman
 
 sudo apt remove -y \
 libreoffice-base-core libreoffice-core \
@@ -14,43 +11,56 @@ cheese brasero shotwell transmission-common
 
 sudo apt remove --autoremove gnome-games -y
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 #-----------------------------------------------------------------------------------------------------------
 # vscode
-sudo apt install -y wget gpg
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-rm -f packages.microsoft.gpg
+read -p "Deseja instalar o VS Code? (Y/n): " install_vscode
+install_vscode=${install_vscode:-Y}
+if [[ "$install_vscode" =~ ^[Yy]$ ]]; then
+    sudo apt install -y wget gpg
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+    echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+    rm -f packages.microsoft.gpg
 
-sudo apt install -y apt-transport-https
-sudo apt update
-sudo apt install -y code # or code-insiders
+    sudo apt install -y apt-transport-https
+    sudo apt update
+    sudo apt install -y code # or code-insiders
+fi
 
 #-----------------------------------------------------------------------------------------------------------
 # docker
-# Add Docker's official GPG key:
-sudo apt update
-sudo apt install -y ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-sudo systemctl enable docker
-sudo systemctl start docker
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+# read -p "Deseja instalar o Docker? (Y/n): " install_docker
+# install_docker=${install_docker:-Y}
+# if [[ "$install_docker" =~ ^[Yy]$ ]]; then
+#     sudo apt update
+#     sudo apt install -y ca-certificates curl
+#     sudo install -m 0755 -d /etc/apt/keyrings
+#     sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+#     sudo chmod a+r /etc/apt/keyrings/docker.asc
+#     sudo systemctl enable docker
+#     sudo systemctl start docker
 
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#     echo \
+#       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+#       $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+#       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#     sudo apt-get update
+
+#     sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# fi
+
 #-----------------------------------------------------------------------------------------------------------
-#flatpak
-sudo apt install -y flatpak gnome-software-plugin-flatpak
-sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+# flatpak
+read -p "Deseja instalar o Flatpak? (Y/n): " install_flatpak
+install_flatpak=${install_flatpak:-Y}
+if [[ "$install_flatpak" =~ ^[Yy]$ ]]; then
+    sudo apt install -y flatpak gnome-software-plugin-flatpak
+    sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-flatpak -y install \
-onlyoffice bitwarden
+    flatpak -y install \
+    onlyoffice bitwarden
+fi
+
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
