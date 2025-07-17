77ead936b795df00743918ab42d40a43e3328f66
Ticiano Diniz <ticciano@gmail.com>
2025-07-17 12:11:05 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,25 +30,25 @@ fi
 #-----------------------------------------------------------------------------------------------------------
 # docker
 
-# read -p "Deseja instalar o Docker? (Y/n): " install_docker
-# install_docker=${install_docker:-Y}
-# if [[ "$install_docker" =~ ^[Yy]$ ]]; then
-#     sudo apt update
-#     sudo apt install -y ca-certificates curl
-#     sudo install -m 0755 -d /etc/apt/keyrings
-#     sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-#     sudo chmod a+r /etc/apt/keyrings/docker.asc
-#     sudo systemctl enable docker
-#     sudo systemctl start docker
+read -p "Deseja instalar o Docker? (Y/n): " install_docker
+install_docker=${install_docker:-Y}
+if [[ "$install_docker" =~ ^[Yy]$ ]]; then
+    sudo apt update
+    sudo apt install -y ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    sudo systemctl enable docker
+    sudo systemctl start docker
 
-#     echo \
-#       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-#       $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-#       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#     sudo apt-get update
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
 
-#     sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-# fi
+    sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
 
 #-----------------------------------------------------------------------------------------------------------
 # flatpak
